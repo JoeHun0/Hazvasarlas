53ad734e1108fb9fc263e5291e3492bf79325a87
Diak <Diak@T018CG13TEACH>
2018-11-09 13:04:23 +0100

    Initial commit

---
+++ src/hazvasarlas/haz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hazvasarlas;
+
+/**
+ *
+ * @author Diak
+ */
+public class haz {
+    private int keruletszam;
+    private int negyzetmeter;
+    private int ossznegyzetmeter;
+    private String allapot;
+    
+    
+}
